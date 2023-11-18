b323bc314797a72545eb14b01617a8556bdce121
Veikkan <pierre.semanne@gmail.com>
2023-11-18 14:28:45 +0100

    add buy modal

--- packages/client/src/ui/theme/Tile.tsx
+++ packages/client/src/ui/theme/Tile.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import grass from '/assets/tiles/grass.jpg';
 import house1 from '/assets/tiles/house_1.png';
+import BuyPage from './BuyPage';
 
 export default function Tile({ x, y } : { x: number, y: number }) {
-  return (
-    <div className='h-28 w-28 flex justify-center items-center relative'>
+
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return ( <>
+    <div className='h-28 w-28 flex justify-center items-center relative' onClick={() => { setIsOpen(true)}}>
       <img src={grass} alt="" className='h-28 w-28 absolute top-0 left-0'/>
       {x}, {y}
 
@@ -12,5 +16,11 @@ export default function Tile({ x, y } : { x: number, y: number }) {
         <img src={house1} alt="" className='h-28 w-28 absolute top-0 left-0'/>
       )}
     </div>
+
+    {isOpen && (
+      <BuyPage onClose={() =>  setIsOpen(false)} price={1000}
+      />
+    )}
+    </>
   );
 }
