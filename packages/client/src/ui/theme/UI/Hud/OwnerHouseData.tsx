5fbd192f3d38be4dbe451d971a4b048a93488f8b
Veikkan <pierre.semanne@gmail.com>
2023-11-18 19:53:06 +0100

    if scrollable oswned house list

--- packages/client/src/ui/theme/UI/Hud/OwnerHouseData.tsx
+++ packages/client/src/ui/theme/UI/Hud/OwnerHouseData.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const ownedHouses : string[] = []
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 5; i++) {
   ownedHouses.push('house')
 }
 
 export default function OwnerHouseData() {
   return (
-    <div className='flex place-content-center h-full  pl-8'>
+    <div className='flex place-content-center h-full pl-8'>
       <div className='flex flex-col'>
         <h3 className='text-white uppercase p-6'>Your current Houses</h3>
-        <div className='flex flex-col item'>
+        <div className='flex flex-col items-start overflow-y-scroll max-h-[750px]'>
           {ownedHouses.map((house, index) => (
             <div key={index} className='pl-8 p-2 text-white'>
               {house}
@@ -20,5 +20,5 @@ export default function OwnerHouseData() {
         </div>
       </div>
     </div>
-  )
+  );
 }
