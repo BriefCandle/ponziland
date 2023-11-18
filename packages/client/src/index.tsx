d5e3cb99cd76522899d34c9d011215bd76466785
Veikkan <pierre.semanne@gmail.com>
2023-11-18 12:32:40 +0100

    add tailwind

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./ui/App";
+import "./index.css";
 
 const rootElement = document.getElementById("react-root");
 if (!rootElement) throw new Error("React root not found");
