9f4d7c43d37cc0ecdbdb5fab3da4f7bee6eb00d4
Veikkan <pierre.semanne@gmail.com>
2023-11-18 13:31:07 +0100

    grid dispaly for 64x64 tiles in the front end

--- packages/client/src/ui/theme/Map.tsx
+++ packages/client/src/ui/theme/Map.tsx
@@ -6,21 +6,21 @@ for (let i = 0; i < 64; i++) {
   list.push(i)
 }
 
+const totalWidth = 64 * 7 + 63 * 1;
+
 export default function Map() {
 
     return (
-      <div className='bg-amber-400'>
-        <div className="overflow-x-auto">
-            <div className="grid grid-cols-64 gap-1 ">
+      <div className='overflow-x-auto'>
+      <div className="grid grid-cols-64 gap-4 p-10" style={{ minWidth: `${totalWidth}rem` }}>
             {list.map((x, xi) => (
                 list.map((y, yi) => (
-                <div key={`${xi}-${yi}`} className='bg-red-500 h-30 w-30 flex justify-center items-center'>
+                <div key={`${xi}-${yi}`} className='h-28 w-28 flex border-4 border-slate-900 justify-center items-center '>
                     {x}, {y}
                 </div>
                 ))
             ))}
             </div>
-        </div>
       </div>
     );
   }
