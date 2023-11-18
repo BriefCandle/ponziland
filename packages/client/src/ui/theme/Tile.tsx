5aaf9f6109be9c295cea50b07f338fa3c84b8daf
Veikkan <pierre.semanne@gmail.com>
2023-11-18 14:05:50 +0100

    add houses on top of the tiles

--- packages/client/src/ui/theme/Tile.tsx
+++ packages/client/src/ui/theme/Tile.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import grass from '/assets/tiles/grass.jpg'
-import house1 from '/assets/tiles/house1.png'
+import React from 'react';
+import grass from '/assets/tiles/grass.jpg';
+import house1 from '/assets/tiles/house_1.png';
 
-export default function Tile({ x, y} : {x: number, y: number}) {
+export default function Tile({ x, y } : { x: number, y: number }) {
   return (
-    <div className='h-28 w-28 flex justify-center items-center '>
-      <img src={grass} alt="" className='h-28 w-28'/>
-      
-        {x}, {y}
-  </div>
-  )
+    <div className='h-28 w-28 flex justify-center items-center relative'>
+      <img src={grass} alt="" className='h-28 w-28 absolute top-0 left-0'/>
+      {x}, {y}
+
+      {x != 0 && (
+        <img src={house1} alt="" className='h-28 w-28 absolute top-0 left-0'/>
+      )}
+    </div>
+  );
 }
