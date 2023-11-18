7a78241fe6450d27c03e06aa0c1f604b64f3bbcb
Veikkan <pierre.semanne@gmail.com>
2023-11-18 13:52:50 +0100

    add house sprite add cyan background

--- packages/client/src/ui/theme/Map.tsx
+++ packages/client/src/ui/theme/Map.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import grass from '/assets/tiles/grass.jpg'
+import house1 from '/assets/tiles/house1.png'
 
 const list : number[] = []
 
@@ -12,7 +13,7 @@ const totalWidth = 64 * 7 + 63 * 1;
 export default function Map() {
 
     return (
-      <div className='overflow-x-auto'>
+      <div className='overflow-x-auto bg-[#466567]'>
       <div className="grid grid-cols-64 gap-4 p-10" style={{ minWidth: `${totalWidth}rem` }}>
             {list.map((x, xi) => (
                 list.map((y, yi) => (
