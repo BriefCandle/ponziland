abebe9e7faea385fdff5d9a1c6164e09840f9694
Brief_Kandle <briefkandle@gmail.com>
2023-11-19 03:56:44 +0300

    fix(client): try to fix useNetworkLayer

--- packages/client/src/ui/theme/Main.tsx
+++ packages/client/src/ui/theme/Main.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import Map from './Map';
-import Hud from './UI/Hud/Hud';
-
+import React from "react";
+import Map from "./Map";
+import Hud from "./UI/Hud/Hud";
+import { useMUD } from "../../store";
+import { useEntityQuery } from "@latticexyz/react";
+import { Has } from "@latticexyz/recs";
+import { useNetworkLayer } from "../hooks/useNetworkLayer";
 
 const totalWidth = 64 * 7 + 63 * 1;
 
 export default function Main() {
+  const networkLayer = useNetworkLayer();
+  const Tile = networkLayer?.components.Tile;
+  // const {
+  //   networkLayer: {
+  //     components: { Tile },
+  //   },
+  // } = useMUD();
+  // console.log(useEntityQuery([Has(Tile.)]));
+  console.log("networkLayer", networkLayer?.components.Tile.id);
 
-    return (
-      <div className='overflow-x-auto bg-[#466567]'>
-        <Map />
-        <Hud />
-      </div>
-    );
-  }
-  
+  return (
+    <div className="overflow-x-auto bg-[#466567]">
+      <Map />
+      <Hud />
+    </div>
+  );
+}
